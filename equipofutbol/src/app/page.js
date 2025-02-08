764b15fdf8f392df33350ee3bfa82cff45071be1
Arturo Duarte <arturduar60@gmail.com>
2025-02-08 01:04:20 -0600

    Ejercicios complementarios DPS

--- equipofutbol/src/app/page.js
+++ equipofutbol/src/app/page.js
@@ -1,50 +1,69 @@
-import styles from "./page.module.css"; const Equipos = ({ equipos }) => { return ( 
-  <div className={styles.container__list}> 
-   <h2 className={styles.title}>Equipos de Fútbol</h2> 
-   {equipos.map((equipo) => ( 
-   <div key={equipo.id}> 
-   <h3 className={styles.nameclub}>{equipo.nombre}</h3> 
-   <ul > 
-   {equipo.plantilla.map((jugador) => ( 
-   <li className={styles.container__list} key={jugador.id}> 
-   <strong>{jugador.nombre}</strong> 
-   <p>Altura: {jugador.Altura}m <br></br> Peso: 
-  {jugador.Peso}Kg</p> 
-   </li> 
-   ))} 
-   </ul> 
-   </div> 
-   ))} 
-   </div> 
-  ); 
-  }; 
-   
-   
-  export default function Home() { 
-   // Simula la obtención de datos desde tu JSON 
-   const equiposData = [ 
-   {"id": 1, 
-   "nombre": "Real Madrid", 
-   "plantilla":[ 
-   {"id": 1,"nombre":"Eden Hazard","Altura":"1.75","Peso":"74Kg"}, 
-   {"id": 2,"nombre":"Gonzalo García","Altura":"1.82","Peso":"74Kg"}, 
-   {"id": 3,"nombre":"Karim Benzema","Altura":"1.85","Peso":"81Kg"} 
-  ]}, 
-   {"id": 2, 
-   "nombre": "Barcelona", 
-   "plantilla":[ 
-   {"id": 1,"nombre":"Marc-André ter Stegen ","Altura":"1.75","Peso":"74Kg"}, 
-   {"id": 2,"nombre":"Iñigo Martinez","Altura":"1.82","Peso":"74Kg"}, 
-   {"id": 3,"nombre":"Gavi","Altura":"1.85","Peso":"81Kg"} 
-   ]} 
-   // ... agregar otros equipos 
-  ]; 
-   return ( 
-   <main className={styles.main}> 
-  <div> 
-   <h1>Mi Aplicación de Fútbol</h1> 
-   <Equipos equipos={equiposData} /> 
-   </div> 
-   </main> 
-  ); 
-  } 
+import styles from "./page.module.css";
+
+const Equipos = ({ equipos }) => {
+  return (
+    <div>
+      <h2 className={styles.title}>Equipos de Fútbol</h2>
+      {equipos.map((equipo) => (
+        <div key={equipo.id}>
+          <h3 className={styles.nameclub}>{equipo.nombre}</h3>
+          <ul >
+            {equipo.plantilla.map((jugador) => (
+              <li className={styles.container__equipo} key={jugador.id}>
+                <div className={styles.container__img}><img src={jugador.foto}></img></div>
+                <div>
+                  <strong>{jugador.nombre}</strong>
+                  <p>Altura: {jugador.Altura}m </p>
+                  <p>Peso: {jugador.Peso}</p>
+                </div>         
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+
+export default function Home() {
+  // Simula la obtención de datos desde tu JSON 
+  const equiposData = [
+    {
+      "id": 1,
+      "nombre": "Real Madrid",
+      "plantilla": [
+        { "id": 1, "nombre": "Eden Hazard", "Altura": "1.75", "Peso": "74Kg", "foto": "/assets/images/EdenHazard.jpeg" },
+        { "id": 2, "nombre": "Gonzalo García", "Altura": "1.82", "Peso": "74Kg", "foto": "/assets/images/gonzaloGarcia.jpg" },
+        { "id": 3, "nombre": "Karim Benzema", "Altura": "1.85", "Peso": "81Kg", "foto": "/assets/images/KarimBenzema.jpg" }
+      ]
+    },
+    {
+      "id": 2,
+      "nombre": "Barcelona",
+      "plantilla": [
+        { "id": 1, "nombre": "Marc-André ter Stegen ", "Altura": "1.75", "Peso": "74Kg", "foto": "/assets/images/MarcAndre.jpeg" },
+        { "id": 2, "nombre": "Iñigo Martinez", "Altura": "1.82", "Peso": "74Kg", "foto": "/assets/images/InigoMartinez.jpg" },
+        { "id": 3, "nombre": "Gavi", "Altura": "1.85", "Peso": "81Kg", "foto": "/assets/images/Gavi.jpeg" }
+      ]
+    },
+    {
+      "id": 3,
+      "nombre": "Manchester city",
+      "plantilla": [
+        { "id": 1, "nombre": "Erling Haaland", "Altura": "1.94", "Peso": "84Kg", "foto": "/assets/images/ErlingHaaland.webp" },
+        { "id": 2, "nombre": "Omar Marmoush", "Altura": "1.83", "Peso": "81Kg", "foto": "/assets/images/OmarMarmoush.webp" },
+        { "id": 3, "nombre": "Mateo Kovacic", "Altura": "1.77", "Peso": "75Kg", "foto": "/assets/images/MateoKovacic.jpg" }
+      ]
+    }
+    // ... agregar otros equipos 
+  ];
+  return (
+    <main className={styles.main}>
+      <div>
+        <h1>Mi Aplicación de Fútbol</h1>
+        <Equipos equipos={equiposData} />
+      </div>
+    </main>
+  );
+} 
--- sumaryrestar/src/app/page.js
+++ sumaryrestar/src/app/page.js
@@ -2,34 +2,73 @@
 import { useState } from "react"; import styles from "./page.module.css";
 
 export default function Home() {
-  const [numero1, setNumero1] = useState(''); const [numero2, setNumero2]
-    = useState(''); const [resultado, setResultado] = useState(null);
+  const [numero1, setNumero1] = useState('');
+  const [numero2, setNumero2] = useState('');
+  const [resultado, setResultado] = useState(null);
 
   const sumar = () => {
-    const resultadoSuma = parseFloat(numero1) + parseFloat(numero2); setResultado(`Resultado de la suma: 
-${resultadoSuma}`);
+    const resultadoSuma = parseFloat(numero1) + parseFloat(numero2);
+    setResultado(`Resultado de la suma: ${resultadoSuma}`);
   };
   const restar = () => {
-    const resultadoResta = parseFloat(numero1) - parseFloat(numero2); setResultado(`Resultado de la resta: 
-${resultadoResta}`);
+    const resultadoResta = parseFloat(numero1) - parseFloat(numero2);
+    setResultado(`Resultado de la resta: ${resultadoResta}`);
   };
+
+  const multiplicar = () => {
+    const resultadoMultiplicacion = parseFloat(numero1) * parseFloat(numero2);
+    setResultado(`Resultado de la multiplicación: ${resultadoMultiplicacion}`);
+  }
+
+  const division = () => {
+    const resultadoDivision = parseFloat(numero1) / parseFloat(numero2);
+    if(numero2 == 0){
+      setResultado("La división entre 0 no está definida");
+    } else {
+      setResultado(`Resultado de la división: ${resultadoDivision}`);
+    } 
+  }
+
+  const potenciar = () => {
+    const resultadoPotencia = Math.pow(parseFloat(numero1), parseFloat(numero2));
+    setResultado(`Resultado de la potencia: ${resultadoPotencia}`);
+  }
+
+  const raizCuadrada = () => {
+    const resultadoPotencia = Math.sqrt(parseFloat(numero1));
+    if (numero1 < 0) {
+      setResultado("No se puede obtener la raiz cuadrada de un número negativo");
+    } else{
+      setResultado(`Resultado de la raiz cuadrada del Numero 1: ${resultadoPotencia}`);
+    }
+  }
+
+  const limpiar = () =>{
+    setNumero1('');
+    setNumero2('');
+    setResultado('');
+  }
+
   return (<main className={styles.main}>
     <div className={styles.calculadora}>
       <div className={styles.numeros}>
-        <label className={styles.text}>Número 1:</label> <input className={styles.inputnum}
-          type="number" value={numero1} onChange={(e) => setNumero1(e.target.value)} />
+        <label className={styles.text}>Número 1:</label>
+        <input className={styles.inputnum} type="number" value={numero1} onChange={(e) => setNumero1(e.target.value)} />
       </div>
       <div className={styles.numeros}>
-        <label className={styles.text} >Número 2:</label> <input
-          className={styles.inputnum} type="number" value={numero2} onChange={(e) =>
-            setNumero2(e.target.value)} />
+        <label className={styles.text} >Número 2:</label>
+        <input className={styles.inputnum} type="number" value={numero2} onChange={(e) => setNumero2(e.target.value)} />
       </div>
-      <div>
+      <div className={styles.container__buttons}>
         <button className={styles.button} onClick={sumar}>Sumar</button>
         <button className={styles.button} onClick={restar}>Restar</button>
+        <button className={styles.button} onClick={multiplicar}>Multiplicar</button>
+        <button className={styles.button} onClick={division}>División</button>
+        <button className={styles.button} onClick={potenciar}>Potenciar</button>
+        <button className={styles.button} onClick={raizCuadrada}>Raiz cuadrada</button>
+        <button className={styles.button} onClick={limpiar} name="Limpiar">Limpiar</button>
       </div>
-      {resultado && <div
-        className={styles.resultado}>{resultado}</div>}
+      {resultado && <div className={styles.resultado}>{resultado}</div>}
     </div>
   </main>
   );
